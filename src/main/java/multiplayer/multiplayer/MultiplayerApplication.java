56733090cb2c2f6e75e690a0889327647fdd459e
WWolfsburg <loke.wik@gmail.com>
2026-08-31 14:43:00 +0200

    Initial commit

---
+++ src/main/java/multiplayer/multiplayer/MultiplayerApplication.java
@@ -0,0 +1,13 @@
+package multiplayer.multiplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiplayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiplayerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/multiplayer/multiplayer/MultiplayerApplicationTests.java
@@ -0,0 +1,13 @@
+package multiplayer.multiplayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiplayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
