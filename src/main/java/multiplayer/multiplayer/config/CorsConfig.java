7af3a93eb524c5c2c5c7806751e70f096180aec4
Jonatan Andersson <Jonatan.anderssons@hotmail.com>
2026-09-08 17:16:24 +0200

    Removed unused imports/ added corsconfig / added docker to support deploy

---
+++ src/main/java/multiplayer/multiplayer/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package multiplayer.multiplayer.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer configurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry corsRegistry) {
+                corsRegistry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+        };
+    }
+
+}
--- src/main/java/multiplayer/multiplayer/dto/GameRoomUpdateDTO.java
+++ src/main/java/multiplayer/multiplayer/dto/GameRoomUpdateDTO.java
@@ -1,9 +1,7 @@
 package multiplayer.multiplayer.dto;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import multiplayer.multiplayer.enums.GameState;
 
