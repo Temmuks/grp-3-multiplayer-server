022596c7932812265a1eef2445f9d2075dcf0d45
Robert <boren534@gmail.com>
2026-09-08 11:03:49 +0200

    Refactor createGameRoom method to use CreateGameRoomDTO for better data handling

--- src/main/java/multiplayer/multiplayer/controller/GameRoomController.java
+++ src/main/java/multiplayer/multiplayer/controller/GameRoomController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import multiplayer.multiplayer.Service.GameService;
+import multiplayer.multiplayer.dto.CreateGameRoomDTO;
 import multiplayer.multiplayer.dto.GameRoomDisplayDTO;
 import multiplayer.multiplayer.dto.GameRoomJoinDTO;
 import multiplayer.multiplayer.dto.GameRoomUpdateDTO;
@@ -33,8 +34,8 @@ public class GameRoomController {
 
     // Skapar Gameroom och knyter det till clientens ID
     @PostMapping("/gameRooms")
-    public GameRoomDisplayDTO createGameRoom(@RequestBody String clientId, int maxPlayers) {
-        GameRoom gameRoom = gameService.createGameRoom(clientId, maxPlayers);
+    public GameRoomDisplayDTO createGameRoom(@RequestBody CreateGameRoomDTO createGameRoomDTO) {
+        GameRoom gameRoom = gameService.createGameRoom(createGameRoomDTO);
         GameRoomDisplayDTO gameRoomDisplayDTO = GameRoomMapper.toDisplayDTO(gameRoom);
         return gameRoomDisplayDTO;
     }
