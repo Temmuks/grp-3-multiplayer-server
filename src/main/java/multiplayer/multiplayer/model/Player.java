e43b07c17ec66f37fa8e9345684d70a91ce986b3
williameliasson <williameliasson@users.noreply.github.com>
2026-09-10 15:36:11 +0200

    updated Player model to include dashesLeft, and currentDashTicksLeft fields

--- src/main/java/multiplayer/multiplayer/model/Player.java
+++ src/main/java/multiplayer/multiplayer/model/Player.java
@@ -15,69 +15,92 @@ public class Player {
 
     String color;
 
+    int currentDashTicksLeft = 0;
+
+    int dashesLeft = 3;
+
     // TBD
     // int points;
     // TBD
     // String Powerup;
 
-    public Player(String playerId, int currentX, int currentY, String direction, boolean isAlive, String color) {
+    public Player(String playerId, int currentX, int currentY, String direction, boolean isAlive, String color, int currentDashTicksLeft, int dashesLeft) {
         this.playerId = playerId;
         this.currentX = currentX;
         this.currentY = currentY;
         this.direction = direction;
         this.isAlive = isAlive;
         this.color = color;
+        this.currentDashTicksLeft = currentDashTicksLeft;
+        this.dashesLeft = dashesLeft;
     }
 
     public Player() {
     }
 
+    
     public String getPlayerId() {
         return playerId;
     }
-
+    
     public void setPlayerId(String playerId) {
         this.playerId = playerId;
     }
-
+    
     public int getCurrentX() {
         return currentX;
     }
-
+    
     public void setCurrentX(int currentX) {
         this.currentX = currentX;
     }
-
+    
     public int getCurrentY() {
         return currentY;
     }
-
+    
     public void setCurrentY(int currentY) {
         this.currentY = currentY;
     }
-
+    
     public String getDirection() {
         return direction;
     }
-
+    
     public void setDirection(String direction) {
         this.direction = direction;
     }
-
+    
     public boolean isAlive() {
         return isAlive;
     }
-
+    
     public void setAlive(boolean isAlive) {
         this.isAlive = isAlive;
     }
-
+    
     public String getColor() {
         return color;
     }
-
+    
     public void setColor(String color) {
         this.color = color;
     }
-
+    
+    public int getCurrentDashTicksLeft() {
+        return currentDashTicksLeft;
+    }
+    
+    public void setCurrentDashTicksLeft(int currentDashTicksLeft) {
+        this.currentDashTicksLeft = currentDashTicksLeft;
+    }
+    
+    public int getDashesLeft() {
+        return dashesLeft;
+    }
+    
+    public void setDashesLeft(int dashesLeft) {
+        this.dashesLeft = dashesLeft;
+    }
+    
 }
