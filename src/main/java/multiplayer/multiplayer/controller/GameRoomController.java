de83922ba9016fe4e2c311616be0908d02c07c49
williameliasson <williameliasson@users.noreply.github.com>
2026-09-10 09:27:03 +0200

    added simple DeleteMapping to delete all gameRooms. no auth required for now

--- src/main/java/multiplayer/multiplayer/controller/GameRoomController.java
+++ src/main/java/multiplayer/multiplayer/controller/GameRoomController.java
@@ -3,6 +3,7 @@ package multiplayer.multiplayer.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +24,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 
@@ -84,4 +87,11 @@ public class GameRoomController {
     public GameRoom startGameRoom(@RequestBody SetGameRoomStatusDTO setGameRoomStatusDTO) {
         return gameService.startGameRoom(setGameRoomStatusDTO);
     }
+
+    @DeleteMapping("/gameRooms")
+    public ResponseEntity<Void> deleteGameRooms() {
+        gameService.deleteAllGameRooms();
+        return ResponseEntity.ok().build();
+    }
+    
 }
