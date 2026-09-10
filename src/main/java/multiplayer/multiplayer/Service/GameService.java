2ffa25463dcc8980cc878de204856c63aa26c52a
williameliasson <williameliasson@users.noreply.github.com>
2026-09-10 15:07:36 +0200

    added player collision checks and wall collision checks in hasPlayerCollided method

--- src/main/java/multiplayer/multiplayer/Service/GameService.java
+++ src/main/java/multiplayer/multiplayer/Service/GameService.java
@@ -133,10 +133,20 @@ public class GameService {
         // Returnera map med alla spelare i gameroomets positioner
     }
 
-    private boolean hasPlayerColided(Player player, GameRoom gameRoom) {
+    private boolean hasPlayerCollided(Player player, GameRoom gameRoom) {
         // kolla om spelare har krockat igenom att kolla spelarens position är och
         // jämför med befintliga positioner i gameRoomets lista
-        return false; // temporär return för att kunna sätta igång servern
+        
+        // Skapa DTO att jämföra med
+        PositionDTO playerPosition = new PositionDTO(player.getCurrentX(), player.getCurrentY());
+
+        boolean hasCollidedWithOtherPlayer = gameRoom.getPreviousPositions().containsKey(playerPosition);
+        boolean hasCollidedWithWall = (playerPosition.x() < 0  // left wall
+                                    || playerPosition.x() >= gameRoom.getGridSize()  // right wall
+                                    || playerPosition.y() < 0  // upper wall
+                                    || playerPosition.y() >= gameRoom.getGridSize()); // lower wall
+        
+        return hasCollidedWithOtherPlayer || hasCollidedWithWall;
     }
 
     public Player checkWinner(GameRoom gameRoom) {
@@ -201,7 +211,7 @@ public class GameService {
         gameRoom.setGameRoomStatus(GameState.NOT_STARTED);
         gameRoom.setMaxPlayers(createGameRoomDTO.getMaxPlayers());
         gameRoom.setGameRoomOwner(createGameRoomDTO.getClientId());
-        gameRoom.setGridSize(createGameRoomDTO.getMaxPlayers() * 64);// Sätter gridsize baserat på max antal spelare. 4
+        gameRoom.setGridSize(4 * 64);// Sätter gridsize baserat på max antal spelare. 4
                                                                      // = 256, 10 = 640, 15 =
         // 960 etc.
         String gameRoomId = UUID.randomUUID().toString();
@@ -253,7 +263,7 @@ public class GameService {
 
     public void distributePlayers(String gameRoomId){
         double degreesOffset = 0;
-        int padding = 10; // Minimum amount of 'pixels' from the wall that a player can spawn at
+        int padding = 30; // Minimum amount of 'pixels' from the wall that a player can spawn at
 
         // Get grid size and how many players there are
         GameRoom gameRoom = getGameRoomById(gameRoomId);
